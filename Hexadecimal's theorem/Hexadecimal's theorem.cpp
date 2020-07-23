c148f86ab16155ce64a8195973cb4fe4f0769426
PAiN <PAiN@PAiN-PC>
2020-07-23 11:20:45 +0200

    Getting started

---
+++ Hexadecimal's theorem/Hexadecimal's theorem.cpp
@@ -0,0 +1,20 @@
+//----------------------------------------------------------------------
+//	Project      : Problem Solving Practice
+//	Problem name : Hexadecimal's theorem
+//	Problem URL  : codeforces.com/contest/199/problem/A
+//	Author       : PAiN
+//	
+//---------------------------------------------------------------------
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	cout << n << " 0 0";
+
+}
